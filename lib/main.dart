5f47dd612ddb80166e725218d1914d80fe57b5f9
ReachPooja <reachpoojaprajapat@gmail.com>
2022-09-21 08:54:33 +0530

    feat: injector

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:voice_chat/app/app.dart';
+import 'package:voice_chat/app/injector/injector.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  configureInjector();
+
   runApp(const MyApp());
 }
