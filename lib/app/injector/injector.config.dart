15210059f6e7ec3023d5edda067b46a718c394b5
ReachPooja <reachpoojaprajapat@gmail.com>
2022-09-21 12:49:35 +0530

    feat speech repo

--- lib/app/injector/injector.config.dart
+++ lib/app/injector/injector.config.dart
@@ -9,8 +9,10 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:speech_to_text/speech_to_text.dart' as _i3;
 
+import '../../src/chat/repository/speech/i_speech_repository.dart' as _i4;
+import '../../src/chat/repository/speech/speech_repository.dart' as _i5;
 import '../../src/core/infrastructure/third_party_injectable_module.dart'
-    as _i4; // ignore_for_file: unnecessary_lambdas
+    as _i6; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -27,7 +29,9 @@ _i1.GetIt $initGetIt(
   final thirdPartyInjectableModule = _$ThirdPartyInjectableModule();
   gh.lazySingleton<_i3.SpeechToText>(
       () => thirdPartyInjectableModule.speechToText);
+  gh.lazySingleton<_i4.ISpeechRepository>(
+      () => _i5.SpeechRepository(get<_i3.SpeechToText>()));
   return get;
 }
 
-class _$ThirdPartyInjectableModule extends _i4.ThirdPartyInjectableModule {}
+class _$ThirdPartyInjectableModule extends _i6.ThirdPartyInjectableModule {}
