54e2806d5278b98cff4ebf3e7cb4958326f87222
ReachPooja <reachpoojaprajapat@gmail.com>
2022-09-21 23:30:11 +0530

    feat: hive init

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:voice_chat/app/app.dart';
 import 'package:voice_chat/app/injector/injector.dart';
 import 'package:voice_chat/bootstrap.dart';
+import 'package:voice_chat/src/chat/models/chat/chat.dart';
+import 'package:voice_chat/src/chat/models/conversation/conversation.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   configureInjector();
 
-  bootstrap(MyApp.new);
+  await Hive.initFlutter();
+  Hive
+    ..registerAdapter(ConversationAdapter())
+    ..registerAdapter(ChatAdapter());
+
+  await bootstrap(MyApp.new);
 }
